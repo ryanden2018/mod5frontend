f73272b20df223e8a3ec599123bf694ba91ab6bf
ryanden2018 <denlinger.ryan@gmail.com>
2019-06-30 20:44:54 -0500

    improve proportions

--- src/furnishings/desk.js
+++ src/furnishings/desk.js
@@ -11,27 +11,27 @@ export default class Desk extends Furnishing {
     super(furnishing,colors,brighten);
 
     const material = new THREE.MeshPhongMaterial({color: new THREE.Color(this.red/255,this.green/255,this.blue/255)});
+    var scale = 1.25;
+    
+    const cube = new THREE.Mesh( new THREE.BoxGeometry(1.75*scale,0.5*scale,0.9*scale),material);
+    cube.position.set(0.0,-0.1*scale+1.0*scale,0.0)
+
+    let cylinder1 = new THREE.Mesh( new THREE.CylinderGeometry( 0.1*scale,0.1*scale,0.8*scale,25 ),
+      material ) 
+    cylinder1.position.set(-0.75*scale,-0.5*scale+1*scale,0.3*scale)
+    
+    let cylinder2 = new THREE.Mesh( new THREE.CylinderGeometry( 0.1*scale,0.1*scale,0.8*scale,25 ),
+      material ) 
+    cylinder2.position.set(0.75*scale,-0.5*scale+1*scale,0.3*scale)
+    
+    let cylinder3 = new THREE.Mesh( new THREE.CylinderGeometry( 0.1*scale,0.1*scale,0.8*scale,25 ),
+      material ) 
+    cylinder3.position.set(-0.75*scale,-0.5*scale+1*scale,-0.3*scale)
 
     
-    const cube = new THREE.Mesh( new THREE.BoxGeometry(1.75,0.5,0.9),material);
-    cube.position.set(0.0,-0.1+1.0,0.0)
-
-    let cylinder1 = new THREE.Mesh( new THREE.CylinderGeometry( 0.1,0.1,0.8,25 ),
-      material ) 
-    cylinder1.position.set(-0.75,-0.5+1,0.3)
-    
-    let cylinder2 = new THREE.Mesh( new THREE.CylinderGeometry( 0.1,0.1,0.8,25 ),
-      material ) 
-    cylinder2.position.set(0.75,-0.5+1,0.3)
-    
-    let cylinder3 = new THREE.Mesh( new THREE.CylinderGeometry( 0.1,0.1,0.8,25 ),
-      material ) 
-    cylinder3.position.set(-0.75,-0.5+1,-0.3)
-
-    
-    let cylinder4 = new THREE.Mesh( new THREE.CylinderGeometry( 0.1,0.1,0.8,25 ),
+    let cylinder4 = new THREE.Mesh( new THREE.CylinderGeometry( 0.1*scale,0.1*scale,0.8*scale,25 ),
       material )
-    cylinder4.position.set(0.75,-0.5+1,-0.3)
+    cylinder4.position.set(0.75*scale,-0.5*scale+1*scale,-0.3*scale)
 
     
     this.threeDimMeshes.push(cube);
