36e0bd2ed44554933ea592533ef4de46a7e4560b
ryanden2018 <denlinger.ryan@gmail.com>
2019-07-05 09:55:05 -0500

    heroku and local config

--- src/components/apiurl.js
+++ src/components/apiurl.js
@@ -1,3 +1,3 @@
-const apiurl = "https://ryanmod5backend.herokuapp.com";
+const apiurl = ( (process.env.NODE_HOME && process.env.NODE_HOME.include("heroku")) ? "https://ryanmod5backend.herokuapp.com" : "http://localhost:8000" );
 
 export default apiurl;
--- src/components/newmodal.js
+++ src/components/newmodal.js
@@ -7,7 +7,7 @@ export default class NewModal extends Modal {
   render() {
     return (
       <div style={this.style}>
-        <p>Enter room name (alphanumeric only, no spaces), and size from 1 to 10:</p>
+        <p>Enter room name <span style={{color:"red"}}><b>(alphanumeric only, no spaces)</b></span>, and size from 1 to 10:</p>
         <p><input type="text" id="roomName" placeholder="Room name" />
         <input type="text" id="roomSize" placeholder="Room size" /></p>
         <p><FormButton value="Cancel" handleSubmit={this.props.cancelCallback} />
