425931ff2a13092fab8eee2133375a099ba7a297
ryanden2018 <denlinger.ryan@gmail.com>
2019-07-05 22:27:29 -0500

    naming

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ export default class NavBar extends React.Component {
     return (
       <div style={{zIndex:2,top:"0px",paddingLeft:"50px",paddingTop:"25px",margin:"0px",width:"100%",height:"80px",backgroundColor:"#edcc53",fontFamily:"sans-serif",fontSize:"25pt",fontWeight:"bold"}}>
         
-        Furniture In Motion
+        Space Planner 3D
       </div>
     );
   }
