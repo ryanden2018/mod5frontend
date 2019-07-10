f09afdb7d6eb83f5bd39390cc41135b495448e7a
ryanden2018 <denlinger.ryan@gmail.com>
2019-07-10 10:33:13 -0500

    safari instructions on login page

--- src/routes/login.js
+++ src/routes/login.js
@@ -51,13 +51,20 @@ export default class Login extends React.Component {
       <div style={{width:"100%",paddingLeft:"50px",paddingTop:"20px"}}>
         <FormButton value="Sign Up" handleSubmit={() => this.props.history.push("/signup")} />
         <form onSubmit={this.handleSubmit}>
-        {/*<form method="POST" action={`${apiurl}/api/login`}>*/}
-          <p>Username: <input type="text" id="username" name="username" /></p>
-          <p>Password: <input type="password" id="password" name="password" /></p>
+          <p>Username: <input type="text" id="username" name="username" value="dummy" /></p>
+          <p>Password: <input type="password" id="password" name="password" value="abcdef" /></p>
           <p style={{color:"red"}}><b>{this.state.err}</b></p>
           <p><button type="submit">Login</button></p>
         </form>
-        <p><b>Dummy account:</b> username "dummy", password "abcdef" (without the quotes)</p>
+        <p><b>Dummy account:</b> username "dummy", password "abcdef" (without the quotes).</p>
+        <p><b>Safari users on MacOSX: </b> To use this site, you will need to 
+        select <tt>Preferences</tt> from the <tt>Safari</tt> menu, click on 
+        <tt>Privacy</tt>, and make sure that "Prevent cross-site tracking" is <b>not</b>
+        selected. Then you will need to restart Safari (<tt>Safari &gt; Quit Safari</tt>).
+        This is because the authentication process uses a cookie set from
+        a secondary domain (which I control), and Safari usually prohibits such cookies
+        for privacy reasons.</p>
+        <p><b>Internet Explorer</b> is not supported; please use Edge, Firefox or Chrome.</p>
       </div>
     );
   }
